12f29e2cdafa344fac505d1ae06d58069dcf3375
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-13 13:19:25 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -12,13 +12,13 @@ class Main
 
 		Raylib.initWindow(screenWidth, screenHeight, "raylib [shapes] example - draw circle sector");
 
-		final center:Vector2 = new Vector((Raylib.getScreenWidth() - 300) / 2.0, Raylib.getScreenHeight() / 2.0);
+		final center:Vector2 = new Vector2((Raylib.getScreenWidth() - 300) / 2.0, Raylib.getScreenHeight() / 2.0);
 
-		final outerRadius:Single = 180.0;
-		final startAngle:Single = 0.0;
-		final endAngle:Single = 180.0;
-		final segments:Single = 10.0;
-		final minSegments:Single = 4;
+		var outerRadius:Single = 180.0;
+		var startAngle:Single = 0.0;
+		var endAngle:Single = 180.0;
+		var segments:Single = 10.0;
+		var minSegments:Single = 4;
 
 		Raylib.setTargetFPS(60);
 
@@ -34,13 +34,13 @@ class Main
 			Raylib.drawCircleSector(center, outerRadius, startAngle, endAngle, Math.floor(segments), Raylib.fade(Raylib.MAROON, 0.3));
 			Raylib.drawCircleSectorLines(center, outerRadius, startAngle, endAngle, Math.floor(segments), Raylib.fade(Raylib.MAROON, 0.6));
 
-			Raylib.guiSliderBar(new Rectangle(600, 40, 120, 20), "StartAngle", TextFormat("%.2f", startAngle), cpp.RawPointer.addressOf((startAngle : cpp.Float32)),
+			Raygui.guiSliderBar(new Rectangle(600, 40, 120, 20), "StartAngle", Raylib.textFormat("%.2f", startAngle), cpp.RawPointer.addressOf((startAngle : cpp.Float32)),
 				0, 720);
-			Raylib.guiSliderBar(new Rectangle(600, 70, 120, 20), "EndAngle", TextFormat("%.2f", endAngle), cpp.RawPointer.addressOf((endAngle : cpp.Float32)), 0,
+			Raygui.guiSliderBar(new Rectangle(600, 70, 120, 20), "EndAngle", Raylib.textFormat("%.2f", endAngle), cpp.RawPointer.addressOf((endAngle : cpp.Float32)), 0,
 				720);
-			Raylib.guiSliderBar(new Rectangle(600, 140, 120, 20), "Radius", TextFormat("%.2f", outerRadius), cpp.RawPointer.addressOf((outerRadius : cpp.Float32)),
+			Raygui.guiSliderBar(new Rectangle(600, 140, 120, 20), "Radius", Raylib.textFormat("%.2f", outerRadius), cpp.RawPointer.addressOf((outerRadius : cpp.Float32)),
 				0, 200);
-			Raylib.guiSliderBar(new Rectangle(600, 170, 120, 20), "Segments", TextFormat("%.2f", segments), cpp.RawPointer.addressOf((segments : cpp.Float32)), 0,
+			Raygui.guiSliderBar(new Rectangle(600, 170, 120, 20), "Segments", Raylib.textFormat("%.2f", segments), cpp.RawPointer.addressOf((segments : cpp.Float32)), 0,
 				100);
 
 			minSegments = Math.floor(Math.ceil((endAngle - startAngle) / 90));

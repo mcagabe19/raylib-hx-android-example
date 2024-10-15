6f29f5dbb1b5155182cff06b7a7bda1b4fb81a5b
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-15 21:36:51 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -4,76 +4,90 @@ import Raylib;
 
 class Main
 {
-	//------------------------------------------------------------------------------------
-	// Program main entry point
-	//------------------------------------------------------------------------------------
-	public static function main():Void
-	{
-		// Initialization
-		//--------------------------------------------------------------------------------------
-		final screenWidth:Int = 800;
-		final screenHeight:Int = 450;
+    //------------------------------------------------------------------------------------
+    // Program main entry point
+    //------------------------------------------------------------------------------------
+    public static function main():Void
+    {
+        // Initialization
+        //--------------------------------------------------------------------------------------
+        final screenWidth:Int = 800;
+        final screenHeight:Int = 450;
 
-		Raylib.initWindow(screenWidth, screenHeight, "raylib [models] example - heightmap loading and drawing");
+        Raylib.initWindow(screenWidth, screenHeight, "raylib [models] example - loading gltf animations");
 
-		// Define our custom camera to look into our 3d world
-		final camera:Camera3D = new Camera3D();
-		camera.position = new Vector3(18.0, 21.0, 18.0); // Camera position
-		camera.target = new Vector3(0.0, 0.0, 0.0); // Camera looking at point
-		camera.up = new Vector3(0.0, 1.0, 0.0); // Camera up vector (rotation towards target)
-		camera.fovy = 45.0; // Camera field-of-view Y
-		camera.projection = CAMERA_PERSPECTIVE; // Camera projection type
+        // Define the camera to look into our 3D world
+        final camera:Camera3D = new Camera3D();
+        camera.position = new Vector3(5.0, 5.0, 5.0);             // Camera position
+        camera.target = new Vector3(0.0, 1.5, 0.0);               // Camera looking at point
+        camera.up = new Vector3(0.0, 1.0, 0.0);                   // Camera up vector (rotation towards target)
+        camera.fovy = 45.0;                                       // Camera field-of-view Y
+        camera.projection = CAMERA_PERSPECTIVE;                   // Camera projection type
 
-		final image:Image = Raylib.loadImage("resources/heightmap.png"); // Load heightmap image (RAM)
-		final texture:Texture = Raylib.loadTextureFromImage(image); // Convert image to texture (VRAM)
+        // Load glTF model
+        final model:Model = Raylib.loadModel("resources/robot.glb");
 
-		final mesh:Mesh = Raylib.genMeshHeightmap(image, new Vector3(16, 8, 16)); // Generate heightmap mesh (RAM and VRAM)
-		final model:Model = Raylib.loadModelFromMesh(mesh); // Load model from generated mesh
+        // Load glTF model animations
+        var animsCount:Int = 0;
+        var animationIndex:UInt = 0;
+        var animCurrentFrame:UInt = 0;
+        var modelAnimation:cpp.RawPointer<RayModelAnimation> = Raylib.loadModelAnimations("resources/robot.glb", animsCount);
 
-		model.materials[0].maps[untyped MATERIAL_MAP_ALBEDO].texture = texture; // Set map diffuse texture
+        final position:Vector3 = new Vector3(0.0, 0.0, 0.0);    // Set model position
 
-		final mapPosition:Vector3 = new Vector3(-8.0, 0.0, -8.0); // Define model position
+        Raylib.setTargetFPS(60); // Set our game to run at 60 frames-per-second
+        //--------------------------------------------------------------------------------------
 
-		Raylib.unloadImage(image); // Unload heightmap image from RAM, already uploaded to VRAM
+        // Main game loop
+        while (!Raylib.windowShouldClose()) // Detect window close button or ESC key
+        {
+            // Update
+            //----------------------------------------------------------------------------------
+            Raylib.updateCamera(camera, CAMERA_THIRD_PERSON);
 
-		Raylib.setTargetFPS(60); // Set our game to run at 60 frames-per-second
-		//--------------------------------------------------------------------------------------
+            // Select current animation
+            if (Raylib.isMouseButtonPressed(MOUSE_BUTTON_RIGHT))
+                animationIndex = (animationIndex + 1) % animsCount;
+            else if (Raylib.isMouseButtonPressed(MOUSE_BUTTON_LEFT))
+                animationIndex = (animationIndex + animsCount - 1) % animsCount;
 
-		// Main game loop
-		while (!Raylib.windowShouldClose()) // Detect window close button or ESC key
-		{
-			// Update
-			//----------------------------------------------------------------------------------
-			Raylib.updateCamera(camera, CAMERA_ORBITAL);
-			//----------------------------------------------------------------------------------
+            // Update model animation
+            final anim:ModelAnimation = modelAnimation[animationIndex];
+            animCurrentFrame = (animCurrentFrame + 1) % anim.frameCount;
+            Raylib.updateModelAnimation(model, anim, animCurrentFrame);
+            //----------------------------------------------------------------------------------
 
-			// Draw
-			//----------------------------------------------------------------------------------
-			Raylib.beginDrawing();
+            // Draw
+            //----------------------------------------------------------------------------------
+            Raylib.beginDrawing();
 
-			Raylib.clearBackground(Raylib.RAYWHITE);
+            Raylib.clearBackground(Raylib.RAYWHITE);
 
-			Raylib.beginMode3D(camera);
+            Raylib.beginMode3D(camera);
 
-			Raylib.drawModel(model, mapPosition, 1.0, Raylib.RED);
-			Raylib.drawGrid(20, 1.0);
+            Raylib.drawModel(model, position, 1.0, Raylib.WHITE); // Draw animated model
+            Raylib.drawGrid(10, 1.0); // Draw a grid
 
-			Raylib.endMode3D();
+            Raylib.endMode3D();
 
-			Raylib.drawTexture(texture, Math.floor(screenWidth - texture.width - 20), 20, Raylib.WHITE);
-			Raylib.drawRectangleLines(Math.floor(screenWidth - texture.width - 20), 20, texture.width, texture.height, Raylib.GREEN);
-			Raylib.drawFPS(10, 10);
+            Raylib.drawText("Use the LEFT/RIGHT mouse buttons to switch animation", 10, 10, 20, Raylib.GRAY);
+            Raylib.drawText("Animation: " + anim.name, 10, Raylib.getScreenHeight() - 20, 10, Raylib.DARKGRAY);
 
-			Raylib.endDrawing();
-			//----------------------------------------------------------------------------------
-		}
+            Raylib.endDrawing();
+            //----------------------------------------------------------------------------------
+        }
 
-		// De-Initialization
-		//--------------------------------------------------------------------------------------
-		Raylib.unloadTexture(texture); // Unload texture
-		Raylib.unloadModel(model); // Unload model
+        // De-Initialization
+        //--------------------------------------------------------------------------------------
+        Raylib.unloadModel(model); // Unload model
 
-		Raylib.closeWindow(); // Close window and OpenGL context
-		//--------------------------------------------------------------------------------------
-	}
+        if (modelAnimation != null)
+        {
+            Raylib.unloadModelAnimations(modelAnimation, animsCount); // Unload animations
+            modelAnimation = null;
+        }
+
+        Raylib.closeWindow(); // Close window and OpenGL context
+        //--------------------------------------------------------------------------------------
+    }
 }

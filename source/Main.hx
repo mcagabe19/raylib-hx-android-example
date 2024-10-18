934c9fc17ecfc94c236e08494bf40cf2eb373d67
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-18 17:57:17 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,6 +1,8 @@
 package;
 
 import Raylib;
+import Raymath;
+import RLights;
 
 class Main
 {
@@ -14,26 +16,41 @@ class Main
         final screenWidth:Int = 800;
         final screenHeight:Int = 450;
 
-        Raylib.initWindow(screenWidth, screenHeight, "raylib [models] example - loading gltf animations");
+        Raylib.setConfigFlags(FLAG_MSAA_4X_HINT); // Enable Multi Sampling Anti Aliasing 4x (if available)
+        Raylib.initWindow(screenWidth, screenHeight, "raylib [shaders] example - basic lighting");
 
-        // Define the camera to look into our 3D world
         final camera:Camera3D = new Camera3D();
-        camera.position = new Vector3(5.0, 5.0, 5.0);             // Camera position
-        camera.target = new Vector3(0.0, 1.5, 0.0);               // Camera looking at point
-        camera.up = new Vector3(0.0, 1.0, 0.0);                   // Camera up vector (rotation towards target)
-        camera.fovy = 45.0;                                       // Camera field-of-view Y
-        camera.projection = CAMERA_PERSPECTIVE;                   // Camera projection type
+        camera.position = new Vector3(2.0, 4.0, 6.0); // Camera position
+        camera.target = new Vector3(0.0, 0.5, 0.0); // Camera looking at point
+        camera.up = new Vector3(0.0, 1.0, 0.0); // Camera up vector (rotation towards target)
+        camera.fovy = 45.0; // Camera field-of-view Y
+        camera.projection = CAMERA_PERSPECTIVE; // Camera projection type
 
-        // Load glTF model
-        final model:Model = Raylib.loadModel("resources/robot.glb");
+        // Load plane model from a generated mesh
+        final model:Model = Raylib.loadModelFromMesh(Raylib.genMeshPlane(10.0, 10.0, 3, 3));
+        final cube:Model = Raylib.loadModelFromMesh(Raylib.genMeshCube(2.0, 4.0, 2.0));
 
-        // Load glTF model animations
-        var animsCount:Int = 0;
-        var animationIndex:UInt = 0;
-        var animCurrentFrame:UInt = 0;
-        var modelAnimation:cpp.RawPointer<RayModelAnimation> = Raylib.loadModelAnimations("resources/robot.glb", animsCount);
+        // Load basic lighting shader
+        final shader:Shader = Raylib.loadShader("resources/lighting.vs", "resources/lighting.fs");
 
-        final position:Vector3 = new Vector3(0.0, 0.0, 0.0);    // Set model position
+        // Get some required shader locations
+        shader.locs[untyped SHADER_LOC_VECTOR_VIEW] = Raylib.getShaderLocation(shader, "viewPos");
+
+        // Ambient light level (some basic lighting)
+        Raylib.setShaderValue(shader, Raylib.getShaderLocation(shader, "ambient"), cast utils.FloatConstPointer.fromArray([0.1, 0.1, 0.1, 1.0]),
+            SHADER_UNIFORM_VEC4);
+
+        // Assign out lighting shader to model
+        model.materials[0].shader = shader;
+        cube.materials[0].shader = shader;
+
+        // Create lights
+        final lights:Array<Light> = [];
+
+        lights.push(RLights.createLight(LIGHT_POINT, new Vector3(-2, 1, -2), Raymath.vector3Zero(), Raylib.YELLOW, shader));
+        lights.push(RLights.createLight(LIGHT_POINT, new Vector3(2, 1, 2), Raymath.vector3Zero(), Raylib.RED, shader));
+        lights.push(RLights.createLight(LIGHT_POINT, new Vector3(-2, 1, 2), Raymath.vector3Zero(), Raylib.GREEN, shader));
+        lights.push(RLights.createLight(LIGHT_POINT, new Vector3(2, 1, -2), Raymath.vector3Zero(), Raylib.BLUE, shader));
 
         Raylib.setTargetFPS(60); // Set our game to run at 60 frames-per-second
         //--------------------------------------------------------------------------------------
@@ -43,18 +60,28 @@ class Main
         {
             // Update
             //----------------------------------------------------------------------------------
-            Raylib.updateCamera(camera, CAMERA_THIRD_PERSON);
+            Raylib.updateCamera(camera, CAMERA_ORBITAL);
 
-            // Select current animation
-            if (Raylib.isMouseButtonPressed(MOUSE_BUTTON_RIGHT))
-                animationIndex = (animationIndex + 1) % animsCount;
-            else if (Raylib.isMouseButtonPressed(MOUSE_BUTTON_LEFT))
-                animationIndex = (animationIndex + animsCount - 1) % animsCount;
+            // Update the shader with the camera view vector (points towards { 0.0f, 0.0f, 0.0f })
+            Raylib.setShaderValue(shader, shader.locs[untyped SHADER_LOC_VECTOR_VIEW],
+                cast utils.FloatConstPointer.fromArray([camera.position.x, camera.position.y, camera.position.z]), SHADER_UNIFORM_VEC3);
 
-            // Update model animation
-            final anim:ModelAnimation = modelAnimation[animationIndex];
-            animCurrentFrame = (animCurrentFrame + 1) % anim.frameCount;
-            Raylib.updateModelAnimation(model, anim, animCurrentFrame);
+            // Check key inputs to enable/disable lights
+            if (Raylib.isKeyPressed(KEY_Y))
+                lights[0].enabled = !lights[0].enabled;
+
+            if (Raylib.isKeyPressed(KEY_R))
+                lights[1].enabled = !lights[1].enabled;
+
+            if (Raylib.isKeyPressed(KEY_G))
+                lights[2].enabled = !lights[2].enabled;
+
+            if (Raylib.isKeyPressed(KEY_B))
+                lights[3].enabled = !lights[3].enabled;
+
+            // Update light values (actually, only enable/disable them)
+            for (i in 0...lights.length)
+                RLights.updateLightValues(shader, lights[i]);
             //----------------------------------------------------------------------------------
 
             // Draw
@@ -65,13 +92,25 @@ class Main
 
             Raylib.beginMode3D(camera);
 
-            Raylib.drawModel(model, position, 1.0, Raylib.WHITE); // Draw animated model
-            Raylib.drawGrid(10, 1.0); // Draw a grid
+            Raylib.drawModel(model, Raymath.vector3Zero(), 1.0, Raylib.WHITE);
+            Raylib.drawModel(cube, Raymath.vector3Zero(), 1.0, Raylib.WHITE);
+
+            // Draw spheres to show where the lights are
+            for (i in 0...lights.length)
+            {
+                if (lights[i].enabled)
+                    Raylib.drawSphereEx(lights[i].position, 0.2, 8, 8, lights[i].color);
+                else
+                    Raylib.drawSphereWires(lights[i].position, 0.2, 8, 8, Raylib.colorAlpha(lights[i].color, 0.3));
+            }
+
+            Raylib.drawGrid(10, 1.0);
 
             Raylib.endMode3D();
 
-            Raylib.drawText("Use the LEFT/RIGHT mouse buttons to switch animation", 10, 10, 20, Raylib.GRAY);
-            Raylib.drawText("Animation: " + anim.name, 10, Raylib.getScreenHeight() - 20, 10, Raylib.DARKGRAY);
+            Raylib.drawFPS(10, 10);
+
+            Raylib.drawText("Use keys [Y][R][G][B] to toggle lights", 10, 40, 20, Raylib.DARKGRAY);
 
             Raylib.endDrawing();
             //----------------------------------------------------------------------------------
@@ -79,13 +118,9 @@ class Main
 
         // De-Initialization
         //--------------------------------------------------------------------------------------
-        Raylib.unloadModel(model); // Unload model
-
-        if (modelAnimation != null)
-        {
-            Raylib.unloadModelAnimations(modelAnimation, animsCount); // Unload animations
-            modelAnimation = null;
-        }
+        Raylib.unloadModel(model); // Unload the model
+        Raylib.unloadModel(cube); // Unload the model
+        Raylib.unloadShader(shader); // Unload shader
 
         Raylib.closeWindow(); // Close window and OpenGL context
         //--------------------------------------------------------------------------------------

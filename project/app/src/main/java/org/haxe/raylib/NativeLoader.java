ffb5570a3cb295efba32231db6345b172aae8a2e
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-20 23:39:13 +0300

    Update NativeLoader.java

--- project/app/src/main/java/org/haxe/raylib/NativeLoader.java
+++ project/app/src/main/java/org/haxe/raylib/NativeLoader.java
@@ -1,11 +1,8 @@
 package org.haxe.raylib;
 
-import android.app.AlertDialog;
 import android.app.NativeActivity;
-import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.view.WindowManager;
 
 public class NativeLoader extends NativeActivity
@@ -13,8 +10,6 @@ public class NativeLoader extends NativeActivity
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
-		// getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
 		{
 			getWindow().getAttributes().layoutInDisplayCutoutMode =
@@ -26,33 +21,6 @@ public class NativeLoader extends NativeActivity
 				WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
 		}
 
-		try
-		{
-			super.onCreate(savedInstanceState);
-		}
-		catch (UnsatisfiedLinkError e)
-		{
-			System.err.println(e.getMessage());
-		}
-		catch (Exception e)
-		{
-			System.err.println(e.getMessage());
-		}
+		super.onCreate(savedInstanceState);
 	}
-
-	/*@Override
-	public void onWindowFocusChanged(boolean hasFocus)
-	{
-		super.onWindowFocusChanged(hasFocus);
-
-		if (hasFocus)
-		{
-			getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN |
-				View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-				View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-				View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-				View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-				View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-		}
-	}*/
 }

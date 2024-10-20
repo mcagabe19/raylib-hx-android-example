68628994d9be58af17f935c53b4ed849bc9376ac
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-20 23:58:29 +0300

    Update NativeLoader.java

--- project/app/src/main/java/org/haxe/raylib/NativeLoader.java
+++ project/app/src/main/java/org/haxe/raylib/NativeLoader.java
@@ -3,6 +3,7 @@ package org.haxe.raylib;
 import android.app.NativeActivity;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class NativeLoader extends NativeActivity
@@ -10,6 +11,13 @@ public class NativeLoader extends NativeActivity
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN |
+			View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+			View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+			View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+			View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+			View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
 		{
 			getWindow().getAttributes().layoutInDisplayCutoutMode =
@@ -22,5 +30,21 @@ public class NativeLoader extends NativeActivity
 		}
 
 		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus)
+	{
+		super.onWindowFocusChanged(hasFocus);
+
+		if (hasFocus)
+		{
+			getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN |
+				View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+				View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+				View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+				View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+				View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+		}
 	}
 }

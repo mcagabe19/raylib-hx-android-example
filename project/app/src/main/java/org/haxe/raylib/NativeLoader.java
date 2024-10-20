d6c1ad49354addfbe25e8ee419069f23663a223e
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-20 23:24:58 +0300

    Update NativeLoader.java

--- project/app/src/main/java/org/haxe/raylib/NativeLoader.java
+++ project/app/src/main/java/org/haxe/raylib/NativeLoader.java
@@ -6,8 +6,6 @@ import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
 import android.view.WindowManager;
 
 public class NativeLoader extends NativeActivity
@@ -18,19 +16,22 @@ public class NativeLoader extends NativeActivity
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
-		setImmersiveMode();
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN |
+			View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+			View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+			View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+			View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+			View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
 		{
-			WindowManager.LayoutParams params = getWindow().getAttributes();
-			params.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS;
-			getWindow().setAttributes(params);
+			getWindow().getAttributes().layoutInDisplayCutoutMode =
+				WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS;
 		}
 		else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
 		{
-			WindowManager.LayoutParams params = getWindow().getAttributes();
-			params.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-			getWindow().setAttributes(params);
+			getWindow().getAttributes().layoutInDisplayCutoutMode =
+				WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
 		}
 
 		try
@@ -39,27 +40,49 @@ public class NativeLoader extends NativeActivity
 		}
 		catch (UnsatisfiedLinkError e)
 		{
-			handleLoadingError(e.getMessage());
+			System.err.println(e.getMessage());
+
+			brokenLibraries = true;
+
+			errorMsgBrokenLib = e.getMessage();
 		}
 		catch (Exception e)
 		{
-			handleLoadingError(e.getMessage());
+			System.err.println(e.getMessage());
+
+			brokenLibraries = true;
+
+			errorMsgBrokenLib = e.getMessage();
+		}
+
+		if (brokenLibraries)
+		{
+			AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+			dlgAlert.setMessage("An error occurred while trying to start the application. Please try again and/or reinstall."
+					    + System.getProperty("line.separator")
+					    + System.getProperty("line.separator")
+					    + "Error: " + errorMsgBrokenLib);
+			dlgAlert.setTitle("Loading Error");
+			dlgAlert.setPositiveButton("Exit", new DialogInterface.OnClickListener()
+			{
+				@Override
+				public void onClick(DialogInterface dialog, int id)
+				{
+					finish();
+				}
+			});
+			dlgAlert.setCancelable(false);
+			dlgAlert.create().show();
 		}
 	}
 
-	private void setImmersiveMode()
+	// Handling loss and regain of application focus
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus)
 	{
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
-		{
-			final WindowInsetsController controller = getWindow().getInsetsController();
+		super.onWindowFocusChanged(hasFocus);
 
-			if (controller != null)
-			{
-				controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
-				controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
-			}
-		}
-		else
+		if (hasFocus)
 		{
 			getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN |
 				View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
@@ -68,38 +91,5 @@ public class NativeLoader extends NativeActivity
 				View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
 				View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 		}
-	}
-
-	private void handleLoadingError(String errorMessage)
-	{
-		brokenLibraries = true;
-
-		errorMsgBrokenLib = errorMessage;
-
-		AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
-		dlgAlert.setMessage("An error occurred while trying to start the application. Please try again and/or reinstall."
-				    + System.getProperty("line.separator")
-				    + System.getProperty("line.separator")
-				    + "Error: " + errorMsgBrokenLib);
-		dlgAlert.setTitle("Loading Error");
-		dlgAlert.setPositiveButton("Exit", new DialogInterface.OnClickListener()
-		{
-			@Override
-			public void onClick(DialogInterface dialog, int id)
-			{
-				finish();
-			}
-		});
-		dlgAlert.setCancelable(false);
-		dlgAlert.create().show();
-	}
-
-	@Override
-	public void onWindowFocusChanged(boolean hasFocus)
-	{
-		super.onWindowFocusChanged(hasFocus);
-
-		if (hasFocus)
-			setImmersiveMode();
 	}
 }

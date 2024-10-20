97fc0bd5a5115515870096c4d54a3f913ae28cda
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-20 23:18:17 +0300

    Update NativeLoader.java

--- project/app/src/main/java/org/haxe/raylib/NativeLoader.java
+++ project/app/src/main/java/org/haxe/raylib/NativeLoader.java
@@ -6,6 +6,8 @@ import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.view.WindowManager;
 
 public class NativeLoader extends NativeActivity
@@ -13,28 +15,22 @@ public class NativeLoader extends NativeActivity
 	private boolean brokenLibraries = false;
 	private String errorMsgBrokenLib = "";
 
-	// Loading method of your native application
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
-		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN |
-			View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-			View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-			View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-			View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-			View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+		setImmersiveMode();
 
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
 		{
-			getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-			getWindow().getAttributes().layoutInDisplayCutoutMode =
-				WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS;
+			WindowManager.LayoutParams params = getWindow().getAttributes();
+			params.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS;
+			getWindow().setAttributes(params);
 		}
 		else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
 		{
-			getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-			getWindow().getAttributes().layoutInDisplayCutoutMode =
-				WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+			WindowManager.LayoutParams params = getWindow().getAttributes();
+			params.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+			getWindow().setAttributes(params);
 		}
 
 		try
@@ -43,51 +39,28 @@ public class NativeLoader extends NativeActivity
 		}
 		catch (UnsatisfiedLinkError e)
 		{
-			System.err.println(e.getMessage());
-
-			brokenLibraries = true;
-
-			errorMsgBrokenLib = e.getMessage();
+			handleLoadingError(e.getMessage());
 		}
 		catch (Exception e)
 		{
-			System.err.println(e.getMessage());
-
-			brokenLibraries = true;
-
-			errorMsgBrokenLib = e.getMessage();
-		}
-
-		if (brokenLibraries)
-		{
-			AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
-			dlgAlert.setMessage("An error occurred while trying to start the application. Please try again and/or reinstall."
-					    + System.getProperty("line.separator")
-					    + System.getProperty("line.separator")
-					    + "Error: " + errorMsgBrokenLib);
-			dlgAlert.setTitle("Loading Error");
-			dlgAlert.setPositiveButton("Exit", new DialogInterface.OnClickListener()
-			{
-				@Override
-				public void onClick(DialogInterface dialog, int id)
-				{
-					finish();
-				}
-			});
-			dlgAlert.setCancelable(false);
-			dlgAlert.create().show();
+			handleLoadingError(e.getMessage());
 		}
 	}
 
-	// Handling loss and regain of application focus
-	@Override
-	public void onWindowFocusChanged(boolean hasFocus)
+	private void setImmersiveMode()
 	{
-		super.onWindowFocusChanged(hasFocus);
-
-		if (hasFocus)
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
 		{
-			// If the app has focus, re-enable immersive mode
+			final WindowInsetsController controller = getWindow().getInsetsController();
+
+			if (controller != null)
+			{
+				controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+				controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+			}
+		}
+		else
+		{
 			getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN |
 				View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
 				View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
@@ -95,5 +68,38 @@ public class NativeLoader extends NativeActivity
 				View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
 				View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 		}
+	}
+
+	private void handleLoadingError(String errorMessage)
+	{
+		brokenLibraries = true;
+
+		errorMsgBrokenLib = errorMessage;
+
+		AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+		dlgAlert.setMessage("An error occurred while trying to start the application. Please try again and/or reinstall."
+				    + System.getProperty("line.separator")
+				    + System.getProperty("line.separator")
+				    + "Error: " + errorMsgBrokenLib);
+		dlgAlert.setTitle("Loading Error");
+		dlgAlert.setPositiveButton("Exit", new DialogInterface.OnClickListener()
+		{
+			@Override
+			public void onClick(DialogInterface dialog, int id)
+			{
+				finish();
+			}
+		});
+		dlgAlert.setCancelable(false);
+		dlgAlert.create().show();
+	}
+
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus)
+	{
+		super.onWindowFocusChanged(hasFocus);
+
+		if (hasFocus)
+			setImmersiveMode();
 	}
 }

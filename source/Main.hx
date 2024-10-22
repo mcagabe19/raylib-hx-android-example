443e426116267658e3181e9755a047a6da3ab172
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-22 11:38:57 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,56 +1,118 @@
 package;
 
 import Raylib;
-import Raygui;
-import Raymob;
+
+//------------------------------------------------------------------------------------------
+// Types and Structures Definition
+//------------------------------------------------------------------------------------------
+enum GameScreen
+{
+    LOGO;
+    TITLE;
+    GAMEPLAY;
+    ENDING;
+}
 
 class Main
 {
-	public static function main():Void
-	{
-		final screenWidth:Int = 800;
-		final screenHeight:Int = 450;
+    //------------------------------------------------------------------------------------
+    // Program main entry point
+    //------------------------------------------------------------------------------------
+    public static function main():Void
+    {
+        // Initialization
+        //--------------------------------------------------------------------------------------
+        final screenWidth:Int = 800;
+        final screenHeight:Int = 450;
 
-		Raylib.initWindow(screenWidth, screenHeight, "raylib [shapes] example - draw circle sector");
+        Raylib.initWindow(screenWidth, screenHeight, "raylib [core] example - basic screen manager");
 
-		final center:Vector2 = new Vector2((Raylib.getScreenWidth() - 300) / 2.0, Raylib.getScreenHeight() / 2.0);
+        var currentScreen:GameScreen = GameScreen.LOGO;
 
-		var outerRadius:Single = 180.0;
-		var startAngle:Single = 0.0;
-		var endAngle:Single = 180.0;
-		var segments:Single = 10.0;
-		var minSegments:Single = 4;
+        // TODO: Initialize all required variables and load all required data here!
+        var framesCounter:Int = 0; // Useful to count frames
 
-		Raylib.setTargetFPS(60);
+        Raylib.setTargetFPS(60); // Set desired framerate (frames-per-second)
+        //--------------------------------------------------------------------------------------
 
-		while (!Raylib.windowShouldClose())
-		{
-			Raymob.vibrate(1);
+        // Main game loop
+        while (!Raylib.windowShouldClose()) // Detect window close button or ESC key
+        {
+            // Update
+            //----------------------------------------------------------------------------------
+            switch (currentScreen)
+            {
+                case GameScreen.LOGO:
+                    // TODO: Update LOGO screen variables here!
+                    framesCounter++; // Count frames
 
-			Raylib.beginDrawing();
+                    // Wait for 2 seconds (120 frames) before jumping to TITLE screen
+                    if (framesCounter > 120)
+                    {
+                        currentScreen = GameScreen.TITLE;
+                    }
+                case GameScreen.TITLE:
+                    // TODO: Update TITLE screen variables here!
+                    // Press enter to change to GAMEPLAY screen
+                    if (Raylib.isKeyPressed(KEY_ENTER) || Raylib.isGestureDetected(GESTURE_TAP))
+                    {
+                        currentScreen = GameScreen.GAMEPLAY;
+                    }
+                case GameScreen.GAMEPLAY:
+                    // TODO: Update GAMEPLAY screen variables here!
+                    // Press enter to change to ENDING screen
+                    if (Raylib.isKeyPressed(KEY_ENTER) || Raylib.isGestureDetected(GESTURE_TAP))
+                    {
+                        currentScreen = GameScreen.ENDING;
+                    }
+                case GameScreen.ENDING:
+                    // TODO: Update ENDING screen variables here!
+                    // Press enter to return to TITLE screen
+                    if (Raylib.isKeyPressed(KEY_ENTER) || Raylib.isGestureDetected(GESTURE_TAP))
+                    {
+                        currentScreen = GameScreen.TITLE;
+                    }
+            }
+            //----------------------------------------------------------------------------------
 
-			Raylib.clearBackground(Raylib.RAYWHITE);
+            // Draw
+            //----------------------------------------------------------------------------------
+            Raylib.beginDrawing();
+            Raylib.clearBackground(Raylib.RAYWHITE);
 
-			Raylib.drawLine(500, 0, 500, Raylib.getScreenHeight(), Raylib.fade(Raylib.LIGHTGRAY, 0.6));
-			Raylib.drawRectangle(500, 0, Raylib.getScreenWidth() - 500, Raylib.getScreenHeight(), Raylib.fade(Raylib.LIGHTGRAY, 0.3));
+            switch (currentScreen)
+            {
+                case GameScreen.LOGO:
+                    // TODO: Draw LOGO screen here!
+                    Raylib.drawText("LOGO SCREEN", 20, 20, 40, Raylib.LIGHTGRAY);
+                    Raylib.drawText("WAIT for 2 SECONDS...", 290, 220, 20, Raylib.GRAY);
+                case GameScreen.TITLE:
+                    // TODO: Draw TITLE screen here!
+                    Raylib.drawRectangle(0, 0, screenWidth, screenHeight, Raylib.GREEN);
+                    Raylib.drawText("TITLE SCREEN", 20, 20, 40, Raylib.DARKGREEN);
+                    Raylib.drawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 20, Raylib.DARKGREEN);
+                case GameScreen.GAMEPLAY:
+                    // TODO: Draw GAMEPLAY screen here!
+                    Raylib.drawRectangle(0, 0, screenWidth, screenHeight, Raylib.PURPLE);
+                    Raylib.drawText("GAMEPLAY SCREEN", 20, 20, 40, Raylib.MAROON);
+                    Raylib.drawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, Raylib.MAROON);
+                case GameScreen.ENDING:
+                    // TODO: Draw ENDING screen here!
+                    Raylib.drawRectangle(0, 0, screenWidth, screenHeight, Raylib.BLUE);
+                    Raylib.drawText("ENDING SCREEN", 20, 20, 40, Raylib.DARKBLUE);
+                    Raylib.drawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN", 120, 220, 20, Raylib.DARKBLUE);
+                default:
+            }
 
-			Raylib.drawCircleSector(center, outerRadius, startAngle, endAngle, Math.floor(segments), Raylib.fade(Raylib.MAROON, 0.3));
-			Raylib.drawCircleSectorLines(center, outerRadius, startAngle, endAngle, Math.floor(segments), Raylib.fade(Raylib.MAROON, 0.6));
+            Raylib.endDrawing();
+            //----------------------------------------------------------------------------------
+        }
 
-			Raygui.guiSliderBar(new Rectangle(600, 40, 120, 20), "StartAngle", Std.string(startAngle), startAngle, 0, 720);
-			Raygui.guiSliderBar(new Rectangle(600, 70, 120, 20), "EndAngle", Std.string(endAngle), endAngle, 0, 720);
-			Raygui.guiSliderBar(new Rectangle(600, 140, 120, 20), "Radius", Std.string(outerRadius), outerRadius, 0, 200);
-			Raygui.guiSliderBar(new Rectangle(600, 170, 120, 20), "Segments", Std.string(segments), segments, 0, 100);
+        // De-Initialization
+        //--------------------------------------------------------------------------------------
+        // TODO: Unload all loaded data (textures, fonts, audio) here!
 
-			minSegments = Math.floor(Math.ceil((endAngle - startAngle) / 90));
-
-			Raylib.drawText("MODE: " + ((segments >= minSegments) ? "MANUAL" : "AUTO"), 600, 200, 10, (segments >= minSegments) ? Raylib.MAROON : Raylib.DARKGRAY);
-
-			Raylib.drawFPS(10, 10);
-
-			Raylib.endDrawing();
-		}
-
-		Raylib.closeWindow();
-	}
+        Raylib.closeWindow(); // Close window and OpenGL context
+        //--------------------------------------------------------------------------------------
+    }
 }
